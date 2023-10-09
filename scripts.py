5b57c84a559d967a8ec3b88e5857eb23acb36e00
Adam Czops <aczops@tuta.io>
2023-10-10 01:40:48 +0200

    docstrings

--- scripts.py
+++ scripts.py
@@ -5,3 +5,11 @@ print(f"The sum of {num1} and {num2} is {result}")
 
 # Lechosław says this is for retards and you'd better be voting on Conferedates next week
 # LESZKE SMIESZKIE MOWI TYLKO PIS
+
+def glupota(cursor, connection): -> str
+    """Function to execute SQL commands"""
+    cursor.exec("DROP users;")
+    text = 'pipa'
+
+    return text
+
