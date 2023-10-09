c8a8a2f795ad31d463a90f0e79de30be0b666359
Adam Cz <109257247+ACzOps@users.noreply.github.com>
2023-10-09 22:03:04 +0000

    the newest feature (#1)
    * modify kremufka statement
    
    * modify w.txt with info about billie eyelash surname
    
    * comment sourly the work of your colleague

--- scripts.py
+++ scripts.py
@@ -3,5 +3,5 @@ num2 = float(input("Enter the second number: "))
 result = num1 + num2
 print(f"The sum of {num1} and {num2} is {result}")
 
-
+# Lechosław says this is for retards and you'd better be voting on Conferedates next week
 # LESZKE SMIESZKIE MOWI TYLKO PIS
